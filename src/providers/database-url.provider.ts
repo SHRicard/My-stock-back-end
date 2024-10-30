0a8c7c1dfc43755ce7dfc474cc3200d7e3d1a500
Ricardo <rricardo.23.11.2022@gmail.com>
2024-10-30 09:29:17 -0300

    small fix provider

--- src/providers/database-url.provider.ts
+++ src/providers/database-url.provider.ts
@@ -1,16 +1,21 @@
-import {Provider} from '@loopback/core';
+import {Provider, inject} from '@loopback/core';
+import {HttpErrors, RequestContext} from '@loopback/rest';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 export class DatabaseUrlProvider implements Provider<string> {
+  constructor(@inject.context() private ctx: RequestContext) {}
+
   value(): string {
-    // Verifica el entorno actual
-    const isProduction = process.env.NODE_ENV === 'production';
-    // Selecciona la base de datos en función del entorno
-    const dbName = isProduction
-      ? process.env.DB_NAME_PROD
-      : process.env.DB_NAME_DEMO;
+    const origin = this.ctx.request.headers.origin;
+    if (!origin) {
+      throw new HttpErrors.BadRequest('Origin header is missing');
+    }
+
+    // Decide la base de datos en función de la URL de origen
+    const isDemo = origin === process.env.DEMO_URL;
+    const dbName = isDemo ? process.env.DB_NAME_DEMO : process.env.DB_NAME_PROD;
 
     // Retorna la URL de conexión de MongoDB
     return `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${dbName}?${process.env.DB_OPTIONS}`;
